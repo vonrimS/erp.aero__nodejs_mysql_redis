b224439b3a46cc633dc68d3aceadcfc27fe4a7d3
vonrimS <mr.mytab@gmail.com>
2023-03-22 12:44:26 +0100

    hashing password before save in db

--- routes/auth.js
+++ routes/auth.js
@@ -12,19 +12,39 @@ router.post('/signin/new_token', (req, res) => {
     res.send('[signin/new_token] is working');
 });
 
+
 router.post('/signup', (req, res) => {
     const { email, password } = req.body;
-    User.create({
-        email, password
-    })
-        .then((user) => {
-            res.status(201).json(user);
-        })
-        .catch((err) => {
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
             console.log(err);
-            res.status(400).json({ error: 'Error creating user' });
+            return res.status(500).json({ error: 'Server error' });
+        }
+
+        // Hash the password using the generated salt
+        bcrypt.hash(password, salt, (err, hash) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ error: 'Server error' });
+            }
+
+            // Create a new user with the hashed password
+            User.create({
+                email,
+                password: hash
+            })
+                .then((user) => {
+                    res.status(201).json(user);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(400).json({ error: 'Error creating user' });
+                });
         });
+    });
 });
+
 
 router.get('/info', (req, res) => {
     res.send('[info] is working');
